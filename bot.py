5fb2a78753f9bb892f2b4064f733139186c1152f
Kyle Zhan <20kzhan@gmail.com>
2024-07-13 03:21:00 -0700

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,131 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from typing import Literal, Optional
+
+import time
+
+import logging
+import os
+from dotenv import load_dotenv
+
+import imageio_ffmpeg as ffmpeg
+
+ffmpeg_path = ffmpeg.get_ffmpeg_exe()
+os.environ['PATH'] = os.path.dirname(ffmpeg_path) + os.pathsep + os.environ['PATH']
+
+load_dotenv()
+
+logging.basicConfig(level=logging.DEBUG)
+
+bot = commands.Bot(command_prefix='!',
+                   intents=discord.Intents.all(),
+                   activity=discord.Activity(type=discord.ActivityType.listening, name="brainrot"),
+                   owner_id=843230753734918154)
+
+@bot.event
+async def on_ready():
+    logging.info(f'Logged in as {bot.user.name}')
+    await bot.load_extension('jishaku')
+
+    for filename in os.listdir('cogs'):
+        if filename.endswith('.py') and not filename.startswith("_"):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+def is_owner():
+    def predicate(interaction: discord.Interaction):
+        return interaction.user.id == 843230753734918154
+    return app_commands.check(predicate)
+
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+@bot.command(hidden=True)
+@commands.is_owner()
+async def load(ctx, extension):
+    loaded_cogs = ''
+    if extension != '~':
+        await bot.load_extension(extension)
+        loaded_cogs += f'\U000023eb {extension}'
+    else:
+        for filename in os.listdir('cogs'):
+            if filename.endswith('.py'):
+                await bot.load_extension(f'cogs.{filename[:-3]}')
+                loaded_cogs += f'\U000023eb cogs.{filename[:-3]}\n\n'
+        loaded_cogs = loaded_cogs[:-2]
+    await ctx.send(loaded_cogs)
+
+
+@bot.command(hidden=True)
+@commands.is_owner()
+async def unload(ctx, extension):
+    unloaded_cogs = ''
+    if extension != '~':
+        await bot.unload_extension(extension)
+        unloaded_cogs += f'\U000023ec {extension}'
+    else:
+        for filename in os.listdir('cogs'):
+            if filename.endswith('.py'):
+                await bot.unload_extension(f'cogs.{filename[:-3]}')
+                unloaded_cogs += f'\U000023ec cogs.{filename[:-3]}\n\n'
+        unloaded_cogs = unloaded_cogs[:-2]
+    await ctx.send(unloaded_cogs)
+
+
+@bot.command(hidden=True)
+@commands.is_owner()
+async def reload(ctx, extension="~"):
+    reload_start = time.time()
+    reloaded_cogs = ''
+    if extension != '~':
+        await bot.unload_extension(extension)
+        await bot.load_extension(extension)
+        reloaded_cogs += f'\U0001f502 {extension}'
+    else:
+        for filename in os.listdir('cogs'):
+            if filename.endswith('.py') and not filename.startswith("_"):
+                try:
+                    await bot.unload_extension(f'cogs.{filename[:-3]}')
+                except commands.ExtensionNotLoaded:
+                    pass
+                await bot.load_extension(f'cogs.{filename[:-3]}')
+                reloaded_cogs += f'\U0001f501 cogs.{filename[:-3]}\n\n'
+        reloaded_cogs = reloaded_cogs[:-2]
+    reload_end = time.time()
+    await ctx.send(f'{reloaded_cogs}\nTook {reload_end - reload_start} to reload all')
+
+os.environ['JISHAKU_NO_UNDERSCORE'] = 'true'
+
+if __name__ == "__main__":
+    bot.run(os.getenv('DISCORD_TOKEN'))
---
+++ cogs/vc.py
@@ -0,0 +1,69 @@
+import discord
+from discord.ext import commands, tasks
+from discord import app_commands
+
+import asyncio
+
+from typing import Literal, Optional
+
+async def start(interaction: discord.Interaction):
+    voice_client = interaction.guild.voice_client
+
+    if not voice_client.is_connected():
+        await voice_client.connect()
+    
+    await voice_client.play(discord.FFmpegPCMAudio("skibidiedgerizz.mp3"))
+
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+
+class VCCommands(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        bot.full_stop = False
+
+        self.play_loop.start()
+    
+    def is_owner():
+        def predicate(interaction: discord.Interaction):
+            return interaction.user.id == 843230753734918154
+        return app_commands.check(predicate)
+    
+    @app_commands.command()
+    @is_owner()
+    @app_commands.guild_only()
+    async def join(self, interaction: discord.Interaction, *, channel: discord.VoiceChannel):
+        await channel.connect()
+        await interaction.response.send_message(f"Joined {channel.mention}", ephemeral=True)
+    
+    @app_commands.command()
+    @is_owner()
+    @app_commands.guild_only()
+    async def leave(self, interaction: discord.Interaction):
+        channel = interaction.guild.voice_client.channel
+        await interaction.guild.voice_client.disconnect()
+        await interaction.response.send_message(f"Left {channel.mention}", ephemeral=True)
+    
+    @app_commands.command()
+    @is_owner()
+    async def full_stop(self, interaction: discord.Interaction):
+        self.bot.full_stop = True
+        for c in self.bot.voice_clients:
+            c.stop()
+        await interaction.response.send_message("Stopped all", ephemeral=True)
+    
+    @tasks.loop(seconds=1)
+    async def play_loop(self):
+        if self.bot.full_stop:
+            return
+
+        voice_clients = self.bot.voice_clients
+        for voice_client in voice_clients:
+            if not voice_client.is_playing() and voice_client.is_connected():
+                voice_client.play(discord.FFmpegPCMAudio("skibidiedgerizz.mp3"),
+                                  after=lambda e: print(f'Error: {e}') if e else None)
+    
+
+async def setup(bot: commands.Bot):
+    cog = VCCommands(bot)
+    await bot.add_cog(cog)
