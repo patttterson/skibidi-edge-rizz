ed19806c2f9892c0887a3827dde4992474385092
Kyle Zhan <20kzhan@gmail.com>
2024-07-13 04:07:14 -0700

    ...

--- cogs/vc.py
+++ cogs/vc.py
@@ -69,11 +69,11 @@ class VCCommands(commands.Cog):
         voice_clients = self.bot.voice_clients
 
         if 1246945254972723202 in [guild.id for guild in self.bot.guilds] and \
-            not [c.is_connected() for c in voice_clients if c.channel.id == self.bot.kyuu_channel.id][0]:
+            not [c for c in voice_clients if c.channel.id == self.bot.kyuu_channel.id]:
             await self.bot.kyuu_channel.connect()
         
         if 1069019652023398532 in [guild.id for guild in self.bot.guilds] and \
-            not [c.is_connected() for c in voice_clients if c.channel.id == self.bot.test_channel.id][0]:
+            not [c for c in voice_clients if c.channel.id == self.bot.test_channel.id]:
             await self.bot.test_channel.connect()
 
         for voice_client in voice_clients:
