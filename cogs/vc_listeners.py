1767b0f733af0cec65e56bc91e2aa2dd9b016147
Kyle Zhan <20kzhan@gmail.com>
2024-07-15 04:37:25 -0700

    how does this not work

--- cogs/vc_listeners.py
+++ cogs/vc_listeners.py
@@ -15,7 +15,8 @@ class VCListeners(commands.Cog):
     
     async def handle_disconnect(self, guild):
         if guild.id in self.bot.settings_cache:
-            channel = guild.get_channel(await self.bot.get_settings(guild.id)["base_channel_id"])
+            g_settings = await self.bot.get_settings(guild.id)
+            channel = guild.get_channel(g_settings["base_channel_id"])
             print(self.bot.get_settings(guild.id)["base_channel_id"])
             if channel is not None:
                 await channel.connect()
